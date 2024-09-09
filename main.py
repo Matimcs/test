3a07aaaa778d0e93a3fecd4fb3f2e532d7262137
Matimcs <129080828+Matimcs@users.noreply.github.com>
2024-09-09 20:56:42 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.firefox.firefox_profile import FirefoxProfile
+from selenium.webdriver.firefox.options import Options
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import asyncio
